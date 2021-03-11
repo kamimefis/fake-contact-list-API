cfeb082fd2346cc3ec875c248a7cdd6fa5967ddc
Camila <camilalobos.2307@gmail.com>
2021-03-10 23:48:15 -0300

    POST contact: done

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,38 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			contact: {
+				full_name: "",
+				email: "",
+				phone: "",
+				address: "",
+				agenda_slug: "lila"
+			}
 		},
 		actions: {
-			//(Arrow) Functions that update the Store
-			// Remember to use the scope: scope.state.store & scope.setState()
+			onChangeContact: e => {
+				const store = getStore();
+				const { contact } = store;
+				contact[e.target.name] = e.target.value;
+				setStore({ contact });
+				console.log(e.target.value);
+			},
+			onSubmitContact: async e => {
+				e.preventDefault();
+				const store = getStore();
+				const { contact } = store;
+
+				const config = {
+					method: "POST",
+					headers: { "Content-type": "application/json" },
+					body: JSON.stringify(contact)
+				};
+
+				const res = await fetch("https://assets.breatheco.de/apis/fake/contact/", config);
+				const data = await res.json();
+
+				console.log(data);
+			}
 		}
 	};
 };
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const AddContact = () => {
+	const { store, actions } = useContext(Context);
+	useEffect(() => {
+		
+	}, []);
+
 	return (
 		<div className="container">
 			<div>
 				<h1 className="text-center mt-5">Add a new contact</h1>
-				<form>
+				<form onSubmit={e => actions.onSubmitContact(e)}>
 					<div className="form-group">
 						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" />
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Full Name"
+							name="full_name"
+							value={store.contact.full_name}
+							onChange={e => actions.onChangeContact(e)}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" />
+						<input
+							type="email"
+							className="form-control"
+							placeholder="Enter email"
+							name="email"
+							value={store.contact.email}
+							onChange={e => actions.onChangeContact(e)}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" />
+						<input
+							type="phone"
+							className="form-control"
+							placeholder="Enter phone"
+							name="phone"
+							value={store.contact.phone}
+							onChange={e => actions.onChangeContact(e)}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" />
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter address"
+							name="address"
+							value={store.contact.address}
+							onChange={e => actions.onChangeContact(e)}
+						/>
 					</div>
-					<button type="button" className="btn btn-primary form-control">
+					<button type="submit" className="btn btn-primary form-control">
 						save
 					</button>
 					<Link className="mt-3 w-100 text-center" to="/">
